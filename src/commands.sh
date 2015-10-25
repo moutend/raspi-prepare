50943baa5afae206f7d352798a4b820627f70f91
moutend <moutend@gmail.com>
2015-10-25 20:56:07 +0900

    Delete

--- src/commands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# RPI_ADDR=192.168.2.2
-
-ssh-keygen -t ecdsa -b 521 -f $HOME/.ssh/id_ecdsa.rpi
-sh ./src/setup-local-ssh.sh pi $RPI_ADDR
-echo '[ -d $HOME/.ssh ] || mkdir $HOME/.ssh' | ssh pi@$RPI_ADDR sh
-cat $HOME/.ssh/id_ecdsa.rpi.pub | ssh pi@$RPI_ADDR tee -a '$HOME/.ssh/authorized_keys'
-cat ./src/setup-remote-ssh.sh | ssh pi@$RPI_ADDR sudo sh
